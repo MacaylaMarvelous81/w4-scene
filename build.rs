12e68bb2a210d9d0a5db17beb4539890c3ec0fd5
Jomar Milan <jomarm@jomarm.com>
2025-05-25 09:39:57 -0700

    Build 2bpp sprites

--- build.rs
+++ build.rs
@@ -1,6 +1,81 @@
 use std::{env, fs};
 use std::path::Path;
-use image::{GenericImageView, ImageReader};
+use image::{DynamicImage, GenericImageView, ImageReader, Pixel};
+
+fn convert_1bpp(img: DynamicImage) -> Vec<u8> {
+    let (width, height) = img.dimensions();
+
+    let mut result = Vec::new();
+    for y in 0..height {
+        let mut byte = 0u8;
+        let mut bit = 0;
+
+        for x in 0..width {
+            let pixel = img.get_pixel(x, y);
+
+            if pixel[1] == 255 {
+                byte |= 1 << (7 - bit);
+            }
+
+            bit += 1;
+            if bit == 8 {
+                result.push(byte);
+                byte = 0;
+                bit = 0;
+            }
+        }
+        if bit != 0 {
+            result.push(byte);
+        }
+    }
+
+    result
+}
+
+fn convert_2bpp(img: DynamicImage) -> Vec<u8> {
+    let (width, height) = img.dimensions();
+
+    let mut result = Vec::new();
+    for y in 0..height {
+        let mut byte = 0u8;
+        let mut bit = 0;
+
+        for x in 0..width {
+            let pixel = img.get_pixel(x, y);
+
+            // Arbitrary ARQ4 palette! Why not?
+            match pixel.channels() {
+                [255, 255, 255, _] => {
+                },
+                [103, 114, 169, _] => {
+                    byte |= 1 << (7 - bit);
+                },
+                [58, 50, 119, _] => {
+                    byte |= 1 << (6 - bit);
+                },
+                [0, 0, 0, _] => {
+                    byte |= 1 << (7 - bit);
+                    byte |= 1 << (6 - bit);
+                },
+                _ => {
+                    println!("cargo::warning=unknown color encountered when processing an image");
+                }
+            }
+
+            bit += 2;
+            if bit == 8 {
+                result.push(byte);
+                byte = 0;
+                bit = 0;
+            }
+        }
+        if bit != 0 {
+            result.push(byte);
+        }
+    }
+
+    result
+}
 
 fn main() {
     let out_dir = env::var_os("OUT_DIR").unwrap();
@@ -12,30 +87,26 @@ fn main() {
 
         if path.is_file() && path.extension().is_some_and(|ext| ext == "png") {
             let img = ImageReader::open(path.clone()).unwrap().decode().unwrap();
-            let (width, height) = img.dimensions();
 
-            let mut raw_data = Vec::new();
-            for y in 0..height {
-                let mut byte = 0u8;
-                let mut bit = 0;
-                for x in 0..width {
-                    let pixel = img.get_pixel(x, y);
-                    if pixel[1] == 255 {
-                        byte |= 1 << (7 - bit);
-                    }
-                    bit += 1;
-                    if bit == 8 {
-                        raw_data.push(byte);
-                        byte = 0;
-                        bit = 0;
-                    }
-                }
-                if bit != 0 {
-                    raw_data.push(byte);
-                }
-            }
+            let new_path = Path::new(&out_dir)
+                .join(path.with_extension("raw").file_name().unwrap());
 
-            fs::write(Path::new(&out_dir).join(path.with_extension("raw").file_name().unwrap()), &raw_data).unwrap();
+            fs::write(new_path, convert_1bpp(img)).unwrap();
+        }
+    }
+
+    println!("cargo::rerun-if-changed=resources/2bpp");
+
+    for entry in fs::read_dir("resources/2bpp").unwrap() {
+        let path = entry.unwrap().path();
+
+        if path.is_file() && path.extension().is_some_and(|ext| ext == "png") {
+            let img = ImageReader::open(path.clone()).unwrap().decode().unwrap();
+
+            let new_path = Path::new(&out_dir)
+                .join(path.with_extension("raw").file_name().unwrap());
+
+            fs::write(new_path, convert_2bpp(img)).unwrap();
         }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ mod wasm4;
 use wasm4::*;
 
 const SMILEY: &[u8; 8] = include_bytes!(concat!(env!("OUT_DIR"), "/smiley.raw"));
+const ALL_COL_TEST: &[u8; 16] = include_bytes!(concat!(env!("OUT_DIR"), "/allcoltest.raw"));
 
 #[no_mangle]
 fn update() {
@@ -16,5 +17,9 @@ fn update() {
     }
 
     blit(SMILEY, 76, 76, 8, 8, BLIT_1BPP);
+
     text("Press X to blink", 16, 90);
+
+    unsafe { *DRAW_COLORS = 0x4321 }
+    blit(ALL_COL_TEST, 76 + 8, 76, 8, 8, BLIT_2BPP);
 }
